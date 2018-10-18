a703bf5e4da8bbea8e7ab68f2780bde520206423
Jesper Asplund <jesper.asplund95@gmail.com>
2018-10-18 12:04:43 +0200

    fixed SPA

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.static('www'));
 // Start the web server on port 3000
 app.listen(3000,() => console.log('Listening on port 3000'));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './www/index.html'));
+});
+
 const Sass = require('./sass');
 const config = require('./config.json');
 
