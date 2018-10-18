5d65d1065664e6e06c4ca9bb85c62b690ccefd08
Joel P <joel.pedersen97@gmail.com>
2018-10-18 15:08:44 +0200

    Added sass history file. git ignore style.css

--- www/js/translation.js
+++ www/js/translation.js
@@ -2,14 +2,18 @@ let historyDaa;
 
 $.getJSON('/json/content.json', function (data) {
   historyData = data;
-  createHistory("sv");
+  createHistory("en");
 });
 
 function createHistory(lang) {
   let title = $(".history-title");
+  let titleImg = $(".history-img");
+  let gameplay = $(".history-gameplay");
+  let ports = $(".history-ports");
   let a = historyData.history[lang];
-  title.append("<p>" + a.title + "</p>");
+  title.append("<h3>" + a.title + "</h3>");
   title.append("<p>" + a.desc + "</p>");
-  title.append("<p>" + a.gameplay + "</p>");
-  title.append("<p>" + a.ports + "</p>");
+  titleImg.append('<img src="imgs/arcadeflyer.jpg">');
+  gameplay.append("<p>" + a.gameplay + "</P>");
+  ports.append("<p>" + a.ports + "</p>");
 }
