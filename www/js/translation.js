c86abc9852646c41fceca7d6c926f7f56a2e6057
Joel P <joel.pedersen97@gmail.com>
2018-10-18 15:43:58 +0200

    Finally done with history jQuery

--- www/js/translation.js
+++ www/js/translation.js
@@ -8,12 +8,15 @@ $.getJSON('/json/content.json', function (data) {
 function createHistory(lang) {
   let title = $(".history-title");
   let titleImg = $(".history-img");
-  let gameplay = $(".history-gameplay");
+  let game = $(".history-gameplay");
   let ports = $(".history-ports");
   let a = historyData.history[lang];
+
   title.append("<h3>" + a.title + "</h3>");
   title.append("<p>" + a.desc + "</p>");
   titleImg.append('<img src="imgs/arcadeflyer.jpg">');
-  gameplay.append("<p>" + a.gameplay + "</P>");
+  game.append("<h3>" + a.gametitle + "</h3>");
+  game.append("<p>" + a.gameplay + "</P>");
+  game.append("<h3>" + a.portstitle + "</h3>");
   ports.append("<p>" + a.ports + "</p>");
 }
