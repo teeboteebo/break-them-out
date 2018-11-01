a0589e2c968080e065b02e8ef6bf362787b5cead
Kelvin <kelvinrylik@hotmail.com>
2018-11-01 12:50:50 +0100

    made the game instructions hidden bellow 1000px and added paddle hit detection alpha 0.1 (since the ball is a spinning square it's quite buggy)

--- www/js/game.js
+++ www/js/game.js
@@ -46,14 +46,13 @@ function loadGame() {
   }
 
   function updateGame(deltaTime) {
-    movePaddle(deltaTime);
 
     //This move paddle and ball while paused
     if (paused) {
       ball.$.css('left', (ball.left = paddle.left + paddle.width / 2 - ball.width / 2));
       return;
     }
-
+    movePaddle(deltaTime);
     moveBall(deltaTime);
   }
 
@@ -117,7 +116,16 @@ function loadGame() {
 
   function collisionDetectBallAndPaddle() {
     if (!isRectAOutsideRectB(ball, paddle)) {
+      // this if statement changes the direction the ball goes on the x axis (left and right)
+      // when the ball hits the paddle
+      if (paddle.left + (paddle.width / 2) > ball.left + (ball.width / 2) ){
+      ball.direction.x = -1;
+      }
+      else {
+      ball.direction.x = 1;
+      }
       ball.direction.y *= -1;
+      // ball.direction.x *= 1;
       ball.top = paddle.top - ball.height;
       score += 5;
       updateInterface();
