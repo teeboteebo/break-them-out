2a0aad3b109db0b34a44768bcc3c1bc6c1c66782
Yasmin <minaa0396@gmail.com>
2018-11-05 14:28:30 +0100

    fixed highscore

--- www/js/highscore.js
+++ www/js/highscore.js
@@ -7,36 +7,12 @@ function createHighscore() {
   let a = jsonData;
   let highscore = $(".highscoreList");
 
-  for (i = 1; i <= 10; i++) {
-    if (i === 1) {
-      highscore.append(' <tr class="first-place">' +
-        ' <th scope="row">' + i + '</th>' +
-        ' <td>' + a[i]["name"] + '</td>' +
-        ' <td>' + a[i]["score"] + '</td>' +
-        '</tr>');
-    }
-    else if (i === 2) {
-      highscore.append(' <tr class="second-place">' +
-        ' <th scope="row">' + i + '</th>' +
-        ' <td>' + a[i]["name"] + '</td>' +
-        ' <td>' + a[i]["score"] + '</td>' +
-        '</tr>');
-    }
-    else if (i === 3) {
-      highscore.append(' <tr class="third-place">' +
-        ' <th scope="row">' + i + '</th>' +
-        ' <td>' + a[i]["name"] + '</td>' +
-        ' <td>' + a[i]["score"] + '</td>' +
-        '</tr>');
-    }
-    else {
-
-      highscore.append(' <tr>' +
-        ' <th scope="row">' + i + '</th>' +
-        ' <td>' + a[i]["name"] + '</td>' +
-        ' <td>' + +a[i]["score"] + '</td>' +
-        '</tr>');
-    }
+  for (i = 0; i < 10; i++) {
+    highscore.append(' <tr>' +
+      ' <th scope="row">' + (i+1) + '</th>' +
+      ' <td>' + a[i]["name"] + '</td>' +
+      ' <td>' + +a[i]["score"] + '</td>' +
+      '</tr>');
   }
 }
 
